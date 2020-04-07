51cc6936b57eb05c5405adf748a1e6eea6646717
Santosh Hembram <58513581+HembramBeta777@users.noreply.github.com>
2020-04-07 08:00:36 +0530

    Create even-odd.c

---
+++ Assignment-1/even-odd.c
@@ -0,0 +1,14 @@
+//even or odd
+	#include<stdio.h>
+	int main()
+	{
+		int num;
+		printf("Enter a number:");
+		scanf("%d",&num);
+		if(num%2==0)
+			printf("The entered number %d is an even number",num);
+		else
+			
+			printf("The entered number %d is an odd number",num);
+	}
+		
