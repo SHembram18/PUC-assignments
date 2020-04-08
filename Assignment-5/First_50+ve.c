508cf807b6db5d77c1b2df28bb09c029ec57cd2d
Santosh Hembram <58513581+HembramBeta777@users.noreply.github.com>
2020-04-08 08:23:30 +0530

    Create First_50+ve.c

---
+++ Assignment-5/First_50+ve.c
@@ -0,0 +1,13 @@
+//PROGRAM   :To print the first 50 positive integers
+//FILE      :assign5_1.c
+//CREATED BY:santosh hembram
+//DATED     :20/09/19
+
+	#include<stdio.h>
+	int main()
+	{
+		int i;
+		printf("The first 50 positive integers:");
+		for(i=1;i<=50;i++)
+			printf("%d\t",i);
+	}
