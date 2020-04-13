86aaf97b08037c9ffc37fe2c7dcd53f3e8f9a2ae
Santosh Hembram <58513581+HembramBeta777@users.noreply.github.com>
2020-04-13 10:14:20 +0530

    Create File__details.c

---
+++ File__details.c
@@ -0,0 +1,191 @@
+                                             
+
+					     ALL ASSIGNMENT DETAILS
+					-------------------------------
+					*********************************
+
+				Assignment-1:
+						*Swap the values of two variables
+						*Display a num is Technumber or not
+						*The largest integer among three
+						*Check entered number is even or odd
+
+				*******************************************************************
+				Assignment-2:
+						          CONTROLS STATEMENT
+
+						*Check & display which type of key pressed by user
+						*Alphanumeric or not
+						*convert the aplhabet[upp-low & low-upp]
+						*Find the roots of quadratic equation
+						*Character is vowel or consonant
+				>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+				Assignment-3:
+						*Candidate is elgible for the course or not
+						*Compute the discount & prepare an invoice report
+						*Read three sides of triangle & display the types
+				<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+				Assignment-4:
+						*Menu for 5 basic arithmatic  operation
+						*Check entered key vowel or consonant
+						*Find area of triangle,rectangle,square,circle
+				@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+				Assignment-5:
+							     ARRAY
+
+						*Print the first 50+ve integer
+						*Print the first 30+ve even number
+						*Print the first 30+ve odd number
+						*Print the sum of first 30+ve integer
+						*Print the product of first 10+ve integer
+						*Print the product of all +ve integer ranging from n1 to n2
+						*Print the factorial of an integer
+				##########################################################################
+				Assignment-6:
+						*Print the sum of individual digits of a +ve integer
+						*Print the reverse of a +ve integer
+						*Pallindrome
+						*Prime number
+						*Perfect number
+						*Armstrong number
+						*Strong number
+						*Tech Number
+						*Find the GCD of any two number
+				++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+				Assignmnet-7:
+						*Convert Decimal to its Binary/Octal/Hexa_Decimal
+						*Convert Binary to Decimal
+						*Print the sin series upto nt term
+						*Print the result of cos series upto nth term
+						*Print the multiplication table of a given number
+						*Add the digit of the number till its reduce to single digit 
+						*Find the cube of odd postion digit and & square of even postion digit
+				=======================================================================================
+				Assignment-8:
+							     ARRAY
+
+						*Copy the content of one array into another array in reverse order
+						*Count the even number & odd number present in a array
+						*Store the even number into 2nd array & odd number into 3rd array
+						*Store the digits into array
+						*Print the digits of the number in word
+				$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+				Assignment-9:
+							  ARRAY OPERATIONS[integer]
+					
+						*Insert a number after a given number
+						*Insert a number according to its Precedence
+						*Delete multiple occurence 
+						*Search & delete all the duplicacy
+						*Sort an array in asscending
+				^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+				Assignment-10: 
+	               					   ARRAY & SET
+
+						*Find the largest & smallest number in an array & Print their differnce
+						*Set Implement set union operation using array
+						*Set Intersection operation using array
+						*Set Difference operation using array
+						*Find the 2nd largest & 2nd smallest element using in an array
+						*Input n number of array count the pair of elements whose sum is more than 20
+				________________________________________________________________________________________________
+				Assignmnet-11:
+							  MATRIX[2d-array]
+
+						*Lower triangle of the matrix
+						*Right diagonal of a matrix
+						*Left diagonal of the matrix
+						*Check 2 matrix is symmetric
+				~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+				Assignment-12:
+							    CHARACTER ARRAY
+
+						*Find number of words present in a string
+						*Number of vowels present in a string
+						*Reverse a string without using library function
+						*Insert a character particular of the word in a string
+						*Delete a character from a particular position of the word
+						*Check the word is pallindrome or not
+						*Sort the characters of the word in asccending order
+						*Find the specific character & its number of occurence
+				%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+				Assignmnet-13:
+								CHARACTER ARRAY
+
+						*Find the number of words begin with specific character 
+						*Find no. of words begins & end with specific character
+						*Count the words whose length is more than 3
+						*Pattern  - INDIA
+						*Print the first word of your name
+						*Print the last word of your name
+						*Print the middle word of your name
+						*S.K. Hembram
+						*Print the short name of your name
+						*Print the surname first in your name
+						*Search & replace a word from string
+						*Replace the repeated occurence of specific string
+						*Print the words containing specific alphabet
+						*Print the ASCII equivalent of each character of a string
+				()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()
+				Assignment-14:
+								2D-CHARACTER ARRAY
+
+						*Print the each line in reverse order
+						*Print the lines containing more than 10 characters
+						*Search specific word in each line present in a string
+						*To insert 2nd line in 2D array
+						*Delete 5th line 
+				!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				Assignmnet-15:
+								FUNCTION
+
+						*To perform various arithmatic operation using function
+						*Perform various operation using function
+				/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+				Assignmnet-16:
+								CALL-BY-REFERENCE
+						*Swap two variables using call-by-reference
+						*Find area & perimeter of a circle using call-by-reference
+				[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]][]
+				Assignmnet-17:
+							   	CALL-BY-REFERENCE[arithmatic operation]
+
+						*Input an integer array & performing following task using call-by-reference
+						*Input a string & performing task using call-by-reference 
+				:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+				Assignment-18:
+								RECURSION
+
+						*Find nth power of a number using recursion
+						*Find the fibonacci series
+						*Check the number prime or not
+						*Find the GCD of two number 
+						*Multiplication table
+				;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+				Assignmnet-19:
+							STRUCTURE
+		
+						*Using function performing following task
+				||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+				Assingnment-20:
+							COMMAND LINE ARGUMNET
+
+						*C-L-A  that will create a copy of a file
+						*C-L-A that will rename an exisiting file like mv command
+						*C-L-A that will overwrite 10 characters from specified position
+						*C-L-A that read the file content in a reverse order
+				""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+				Assignment-21:	
+						   	        FILE
+
+						*Create a text file & write student data & display the student data
+						*Display the words of a file whose length is <4
+						*Display the file content in reverse order in each line
+						*Create a binary file & store & display student's data
+				?????????????????????????????????????????????????????????????????????????????????????
+		
+						
+				
+								
+						 
+			
